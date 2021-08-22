52db1f3e4d072ff2eab38619ef9a5712077df7af
Herman M. Lund <hmlund@users.noreply.github.com>
2021-08-22 20:34:28 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,12 @@ import { TilsynList } from "./TilsynList";
 export const App = () => {
   const [showTilsyn, setShowTilsyn] = React.useState(false);
 
-  if (showTilsyn) {
-    return (
-      <ChakraProvider theme={theme}>
-        <Box textAlign="center" fontSize="xl">
-          <TilsynList />
-        </Box>
-      </ChakraProvider>
-    );
-  }
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <Button onClick={() => setShowTilsyn(true)}> Hent tilsyn </Button>
+          {showTilsyn && <TilsynList />}
         </Grid>
       </Box>
     </ChakraProvider>
